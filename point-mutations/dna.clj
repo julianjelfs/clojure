a42de7cf854e154b532106babd166b36f4ca900b
Julian Jelfs <julian.jelfs@gmail.com>
2014-04-19 18:51:30 +0100

    improved point-mutation code

--- point-mutations/dna.clj
+++ point-mutations/dna.clj
@@ -1,13 +1,7 @@
 (ns dna)
 
+(defn- neq [[x y]] (not= x y))
 
-(defn- dist [c1 c2] (if (= c1 c2) 0 1))
+(defn hamming-distance [s1 s2]
+  (count (filter neq (map vector s1 s2))))
 
-
-(defn- count-diffs [v1 v2 c]
-  (if (or (empty? v1) (empty? v2))
-    c
-    (count-diffs (rest v1) (rest v2) (+ c (dist (first v1) (first v2))))))
-
-
-(defn hamming-distance [s1 s2] (count-diffs (vec s1) (vec s2) 0))
